e52ff1190299135213d5688e53013065aa6fcc5d
Mathis Bukowski <mathis.bukowski@epitech.eu>
2025-03-17 16:49:57 +0100

    chore: edit name of parser for lib

---
+++ src/Arcade/DLLoader.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2025
+** Arcade
+** File description:
+** DLLoader
+*/
+
+#include "DLLoader.hpp"
+
+DLLoader::DLLoader()
+{
+}
+
+DLLoader::~DLLoader()
+{
+}
---
+++ src/Arcade/DLLoader.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2025
+** Arcade
+** File description:
+** DLLoader
+*/
+
+#ifndef DLLOADER_HPP_
+#define DLLOADER_HPP_
+
+class DLLoader {
+    public:
+        DLLoader();
+        ~DLLoader();
+
+    protected:
+    private:
+};
+
+#endif /* !DLLOADER_HPP_ */
