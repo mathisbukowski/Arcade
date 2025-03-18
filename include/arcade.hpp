dd0728fe86767028f2ff4fb43174f806dfe0ca39
Mathis Bukowski <mathis.bukowski@epitech.eu>
2025-03-18 14:47:49 +0100

    feat: addinc include folder with arcade.hpp within librarytype enum in it

---
+++ include/arcade.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2025
+** Arcade
+** File description:
+** arcade
+*/
+
+#ifndef ARCADE_HPP_
+#define ARCADE_HPP_
+
+
+namespace arcade {
+    enum  LibraryType {
+        LIBRARY,
+        GAME,
+        UNKNOWN
+    };
+    class DynamicLibraryObject;
+    class DynamicLibraryManager;
+}
+
+
+#endif /* !ARCADE_HPP_ */
