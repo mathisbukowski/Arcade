b3cfaaa8b92ab40a7aebe607adb5d924a8a9a9af
Mathis Bukowski <mathis.bukowski@epitech.eu>
2025-03-26 19:42:16 +0100

    feat(SDLSoundManager): implement sound management classes for audio handling

---
+++ src/Graphics/Sdl/src/SdlSoundManager.hpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2025
+** arcade
+** File description:
+** 03
+*/
+
+#ifndef SDLSOUNDMANAGER_HPP
+#define SDLSOUNDMANAGER_HPP
+
+#include <map>
+
+#include "Arcade.hpp"
+#include "ISound.hpp"
+
+namespace arcade {
+    class SDLSound : public ISound {
+    public:
+        SDLSound();
+        [[nodiscard]] const MySound& getInformations() const override { return _soundInformations; }
+    private:
+        MySound _soundInformations;
+    };
+    class SDLSoundManager : public ISoundManager {
+    public:
+        int load(const std::string& name, const MySound& sound) override;
+        [[nodiscard]] std::shared_ptr<ISound> get(const std::string& name) const override;
+    private:
+        std::map<std::string, std::shared_ptr<SDLSound>> _sounds;
+    };
+};
+
+
+
+#endif //SDLSOUNDMANAGER_HPP
