5ec465a5ef57609a21b05b0673eeac0b3aa85ef5
Valentin Frappart <valentin.frappart@epitech.eu>
2025-04-04 00:46:08 +0200

    feat(SDL): refactor SDLSOUND and implem SoundManager

--- src/Graphics/Sdl/src/SdlSoundManager.cpp
+++ src/Graphics/Sdl/src/SdlSoundManager.cpp
@@ -6,24 +6,59 @@
 */
 
 #include "SdlSoundManager.hpp"
-
-/*
-** EPITECH PROJECT, 2025
-** arcade
-** File description:
-** 03
-*/
-
-#include "SdlSoundManager.hpp"
+#include "SDL2/SDL.h"
 #include <SDL2/SDL_mixer.h>
 #include <iostream>
 
-arcade::SDLSound::SDLSound() : _soundInformations("", 50) { }
+arcade::SDLSound::SDLSound() : _soundInformations(""), _chunk(nullptr), _channel(-1) { }
+
+arcade::SDLSound::~SDLSound()
+{
+    stop();
+    _chunk.reset();
+}
+
+void arcade::SDLSound::play(float volume)
+{
+    if (!_chunk) {
+        _chunk = std::shared_ptr<Mix_Chunk>(
+            Mix_LoadWAV(_soundInformations.getPath().c_str()),
+            Mix_FreeChunk
+        );
+        if (!_chunk) {
+            std::cerr << "Failed to load sound: " << Mix_GetError() << std::endl;
+            return;
+        }
+    }
+    Mix_VolumeChunk(_chunk.get(), static_cast<int>(volume * 128.0f));
+    _channel = Mix_PlayChannel(-1, _chunk.get(), _soundInformations.isLooped() ? -1 : 0);
+    if (_channel == -1)
+        std::cerr << "Failed to play sound: " << Mix_GetError() << std::endl;
+}
+
+void arcade::SDLSound::stop()
+{
+    if (_channel != -1) {
+        Mix_HaltChannel(_channel);
+        _channel = -1;
+    }
+}
+
+arcade::SDLSoundManager::SDLSoundManager()
+{
+    if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+        std::cerr << "SDL could not initialize audio! SDL Error: " << SDL_GetError() << std::endl;
+        return;
+    }
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+        std::cerr << "SDL_mixer could not initialize! SDL_mixer Error: " << Mix_GetError() << std::endl;
+    }
+}
 
 int arcade::SDLSoundManager::load(const std::string& name, const SoundInfos& sound)
 {
     auto newSound = std::make_shared<SDLSound>();
-    newSound->_soundInformations = sound;
+    newSound->setSoundInfos(sound);
 
     if (_sounds.find(name) != _sounds.end())
         return -1;
@@ -42,11 +77,22 @@ std::shared_ptr<arcade::ISound> arcade::SDLSoundManager::get(const std::string& 
 
 void arcade::SDLSoundManager::playSound(const std::string& name, float volume)
 {
-    (void)name;
-    (void)volume;
+    auto it = _sounds.find(name);
+    if (it != _sounds.end()) {
+        auto sound = std::dynamic_pointer_cast<SDLSound>(it->second);
+        if (sound) {
+            sound->play(volume);
+        }
+    }
 }
 
 void arcade::SDLSoundManager::stopSound(const std::string& name)
 {
-    (void)name;
+    auto it = _sounds.find(name);
+    if (it != _sounds.end()) {
+        auto sound = std::dynamic_pointer_cast<SDLSound>(it->second);
+        if (sound) {
+            sound->stop();
+        }
+    }
 }
--- src/Graphics/Sdl/src/SdlSoundManager.hpp
+++ src/Graphics/Sdl/src/SdlSoundManager.hpp
@@ -9,6 +9,8 @@
 #define SDLSOUNDMANAGER_HPP
 
 #include <map>
+#include <SDL2/SDL_stdinc.h>
+#include <SDL2/SDL_mixer.h>
 
 #include "Arcade.hpp"
 #include "ISound.hpp"
@@ -17,19 +19,30 @@ namespace arcade {
     class SDLSound : public ISound {
     public:
         SDLSound();
+        ~SDLSound();
         [[nodiscard]] const SoundInfos& getInformations() const override { return _soundInformations; }
+        void play(float volume);
+        void stop();
+        void setSoundInfos(const SoundInfos& infos) { _soundInformations = infos; }
+
     private:
         SoundInfos _soundInformations;
+        std::shared_ptr<Mix_Chunk> _chunk;
+        int _channel;
     };
+
     class SDLSoundManager : public ISoundManager {
     public:
+        SDLSoundManager();
+
         int load(const std::string& name, const SoundInfos& sound) override;
         [[nodiscard]] std::shared_ptr<ISound> get(const std::string& name) const override;
+        void playSound(const std::string& name, float volume) override;
+        void stopSound(const std::string& name) override;
+
     private:
         std::map<std::string, std::shared_ptr<SDLSound>> _sounds;
     };
 };
-
-
 
 #endif //SDLSOUNDMANAGER_HPP
