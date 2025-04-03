fd9f0ad8691d9adcf40e7a86f176d613978955a6
Valentin Frappart <valentin.frappart@epitech.eu>
2025-04-03 17:07:02 +0200

    feat(SDL): add SDLSOUNDMANAGER and implem getsounds

--- src/Graphics/Sdl/src/SdlSoundManager.cpp
+++ src/Graphics/Sdl/src/SdlSoundManager.cpp
@@ -6,3 +6,50 @@
 */
 
 #include "SdlSoundManager.hpp"
+
+/*
+** EPITECH PROJECT, 2025
+** arcade
+** File description:
+** 03
+*/
+
+#include "SdlSoundManager.hpp"
+#include <SDL2/SDL_mixer.h>
+#include <iostream>
+
+arcade::SDLSound::SDLSound() : _soundInformations("", 50)
+{
+    // Initialisation avec valeurs par défaut
+}
+
+int arcade::SDLSoundManager::load(const std::string& name, const SoundInfos& sound)
+{
+    auto newSound = std::make_shared<SDLSound>();
+    newSound->_soundInformations = sound;
+
+    if (_sounds.find(name) != _sounds.end())
+        return -1;
+
+    _sounds[name] = newSound;
+    return 0;
+}
+
+std::shared_ptr<arcade::ISound> arcade::SDLSoundManager::get(const std::string& name) const
+{
+    auto it = _sounds.find(name);
+    if (it == _sounds.end())
+        return nullptr;
+    return it->second;
+}
+
+void arcade::SDLSoundManager::playSound(const std::string& name, float volume)
+{
+    (void)name;
+    (void)volume;
+}
+
+void arcade::SDLSoundManager::stopSound(const std::string& name)
+{
+    (void)name;
+}
--- src/Graphics/Sdl/src/SdlTextureManager.cpp
+++ src/Graphics/Sdl/src/SdlTextureManager.cpp
@@ -8,8 +8,6 @@
 #include "SdlTextureManager.hpp"
 #include <utility>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_image.h>
 #include <SDL2/SDL_render.h>
 #include <iostream>
 
