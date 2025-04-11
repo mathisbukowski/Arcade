bf54afe8bbfcebe054af5721c932770a53217021
Valentin Frappart <valentin.frappart@epitech.eu>
2025-04-11 10:06:33 +0200

    Fix(IFont): remove const in load function

--- include/IFont.hpp
+++ include/IFont.hpp
@@ -16,7 +16,7 @@
 
 
 namespace arcade {
-    
+
     /**
      * Font properties
      */
@@ -76,7 +76,7 @@ namespace arcade {
      */
     class IFont {
     public:
-        /** 
+        /**
          * Destroy the IFont object
          */
         virtual ~IFont() = default;
@@ -103,7 +103,7 @@ namespace arcade {
          * @param texture Font properties
          * @return int
          */
-        [[nodiscard]] virtual int load(const std::string& name, const Font& font) = 0;
+        [[nodiscard]] virtual int load(const std::string& name, Font& font) = 0;
         /**
          * Get a font
          * @param name Name of the font
